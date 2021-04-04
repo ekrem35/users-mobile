8334142d6b99a3b5ce5081c99567fadee4c6147e
ekrem35 <anliekrem35@gmail.com>
2021-04-04 21:12:35 +0300

    login pincode ui complated

--- app/screens/Login/index.tsx
+++ app/screens/Login/index.tsx
@@ -3,18 +3,36 @@
  */
 
 import React from 'react'
-import { Content, Container, View } from 'native-base'
+import { Content, Container, View, Text } from 'native-base'
 import GradientButton from '../../../components/GradientButton'
 import KeyPad from '../../../components/Keypad'
 
 const Login = () => {
+  const [digits, setDigits] = React.useState<string[]>([])
   return (
     <Container style={{ backgroundColor: '#0D0B2A' }}>
-      <Content contentContainerStyle={{ flex: 1, padding: 32 }}>
-        <View style={{ flex: 1 }}>
-         <KeyPad onPress={(val) => console.log(val)} />
-        </View>
-        <View>
+      <Content contentContainerStyle={{ padding: 32, flex: 1 }}>
+        <View style={{ justifyContent: 'flex-end', flex: 1 }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 60, marginBottom: 10 }}>
+            {digits.map((digit, index) => <Text style={{ color: 'white', fontSize: 16, marginHorizontal: 10 }} key={index}>⚪</Text>)}
+          </View>
+          <KeyPad
+            onPress={(val) => {
+              const newDigits = [...digits]
+              if (val === 'enter') {
+                if (newDigits.length < 1) {
+                  return null
+                }
+                newDigits.splice(newDigits.length - 1, 1)
+                setDigits(newDigits)
+                return null
+              }
+              if (digits.length >= 6) {
+                return null
+              }
+              newDigits.push(val)
+              setDigits(newDigits)
+            }} />
           <GradientButton onPress={() => true} title="ENTER" />
         </View>
       </Content>
