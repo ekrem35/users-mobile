d7067eeb31ab64feb961cdb5e1b621d9388cbc3e
ekrem35 <anliekrem35@gmail.com>
2021-04-04 21:15:27 +0300

    login pincode title added

--- app/screens/Login/index.tsx
+++ app/screens/Login/index.tsx
@@ -12,6 +12,7 @@ const Login = () => {
   return (
     <Container style={{ backgroundColor: '#0D0B2A' }}>
       <Content contentContainerStyle={{ padding: 32, flex: 1 }}>
+        <Text style={{ color: 'white', fontWeight: 'bold' }}>Enter pin-code</Text>
         <View style={{ justifyContent: 'flex-end', flex: 1 }}>
           <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 60, marginBottom: 10 }}>
             {digits.map((digit, index) => <Text style={{ color: 'white', fontSize: 16, marginHorizontal: 10 }} key={index}>⚪</Text>)}
